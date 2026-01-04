813c979576a88d5650d6bfd919abb55cf18d16a3
usfbullsfan <michaeltrimm@mac.com>
2026-01-04 09:42:25 -0500

    Revert "Bug Fix: Persistent PWA install prompts"

--- backend/migrations/add_pwa_install_dismissed.sql
+++
@@ -1,4 +0,0 @@
--- Add PWA install prompt tracking to users table
--- Allows admins to reset all users: UPDATE users SET pwa_install_dismissed = 0;
-
-ALTER TABLE users ADD COLUMN pwa_install_dismissed BOOLEAN DEFAULT 0;
